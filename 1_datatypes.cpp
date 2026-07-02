b925369ea2a1a47a4ec6e66c347f5508e78d4b74
rujula1012 <rsst13579@gmail.com>
2026-07-02 23:19:43 +0530

    Add C++ basics programs

---
+++ 1_datatypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+// Display different C++ data types
+int main() {
+    int age = 20;
+    float height = 5.8f;
+    double pi = 3.14159265359;
+    char grade = 'A';
+    bool isStudent = true;
+    long population = 1400000000;
+    long long stars = 100000000000LL;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Pi: " << pi << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+    cout << "Population: " << population << endl;
+    cout << "Stars: " << stars << endl;
+
+    return 0;
+}
