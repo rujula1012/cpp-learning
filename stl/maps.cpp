43fe25e3380bb9f92dbd4da46544904374b03b76
rujula1012 <rsst13579@gmail.com>
2026-07-17 15:27:54 +0530

    Add maps practice

--- stl/maps.cpp
+++ stl/maps.cpp
@@ -22,6 +22,49 @@ for (int i = 0; i < n; i++)
     }
 
     cout<<"total students:"<<st.size()<<endl;
+    
+    int x;
+    cout<<"Enter the roll no. to search"<<endl;
+    cin>>x;
+    auto it =st.find(x);
+
+    if(it!=st.end()) cout<<"student found:"<<it->second<<endl;
+    else cout<<"student not found!\n";
+
+    cout<<"Enter the student roll no. to update:"<<endl;
+    cin>>x;
+
+    if(st.find(x)!=st.end())
+    {
+        string newname;
+        cout<<"Enter the new name:"<<endl;
+        cin>>newname;
+
+        st[x]=newname;
+        cout<<"updated record!"<<endl;
+    }
+
+    else{
+        cout<<"student not found!\n";
+    }
+    cout << "Student Records:\n";
+    for (auto p : st)
+    {
+        cout << p.first << " -> " << p.second << endl;
+    }
+cout<<"Enter the student roll no. to delete:"<<endl;
+    cin>>x;
+
+    if(st.erase(x))
+    cout<<"Record deleted"<<endl;
+    else
+    cout<<"Student not found!"<<endl;
+
+     cout << "Student Records:\n";
+    for (auto p : st)
+    {
+        cout << p.first << " -> " << p.second << endl;
+    }
 }
 
 
