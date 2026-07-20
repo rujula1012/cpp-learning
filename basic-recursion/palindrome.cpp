671292c91eb48a7d04a09ba4db6e69cb9239cd2d
rujula1012 <rsst13579@gmail.com>
2026-07-21 00:48:12 +0530

    Add recursion problem

--- basic-recursion/palindrome.cpp
+++ basic-recursion/palindrome.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isPalindrome(string s)
+bool helper(string s, int i)
 {
-   
+
+    if (i > s.size() / 2)
+        return true;
+
+    if (s[i] != s[s.size() - i - 1])
+        return false;
+
+    return helper(s, i + 1);
 }
 
-
-
-
-
-
-
+bool isPalindrome(string s)
+{
+    helper(s, 0);
+}
 
 int main()
 {
-  string s;
-  cout<<"Enter the string:";
-  cin>>s;
-  isPalindrome(s);
-  
+    string s;
+    cout << "Enter the string:";
+    cin >> s;
+    bool result = isPalindrome(s);
+    cout << boolalpha << result;
 }
